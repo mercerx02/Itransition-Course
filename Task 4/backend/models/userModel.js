5bedd846abd145b2a34a781fae64b4f3cc03f673
mercerx02 <laptonik322@mail.ru>
2023-08-01 21:33:39 +0300

    mongodb model setup

---
+++ Task 4/backend/models/userModel.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose')
+
+const userSchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, 'please add a name'],
+        uniqie: true
+    },
+    email: {
+        type: String,
+        required: [true, 'please add a email'],
+        uniqie: true
+    },
+    password: {
+        type: String,
+        required: [true, 'please add a password'],
+    },
+    last_login_date: {
+        type: Date,
+        required: true
+    },
+    is_banned: {
+        type: Boolean,
+        required: true,
+        default: false
+    },
+},
+    {
+        timestamps: true
+    })
+
+module.exports = mongoose.model('User', userSchema)
