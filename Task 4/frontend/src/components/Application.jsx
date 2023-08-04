4cd75be270e38e9b62bdbf82011cc7da5742c26c
mercerx02 <laptonik322@mail.ru>
2023-08-04 21:56:11 +0300

    refactor component

--- Task 4/frontend/src/components/Application.jsx
+++ Task 4/frontend/src/components/Application.jsx
@@ -3,12 +3,12 @@ import DashBoard from './DashBoard'
 import NavBar from './NavBar'
 
 
-const Application = ({ users, setCheckBox, banUsers, unBanUsers, deleteUsers, setStatusAllUsers, loggedUser , setUsers}) => {
+const Application = ({ users, setCheckBox, setStatusAllUsers, loggedUser , setUsers}) => {
     return (
         <>
 
             <NavBar loggedUser={loggedUser}></NavBar>
-            <DashBoard loggedUser={loggedUser} setUsers={setUsers} setStatusAllUsers={setStatusAllUsers} deleteUsers={deleteUsers} unBanUsers={unBanUsers} banUsers={banUsers} setCheckBox={setCheckBox} users={users}></DashBoard>
+            <DashBoard loggedUser={loggedUser} setUsers={setUsers} setStatusAllUsers={setStatusAllUsers}  setCheckBox={setCheckBox} users={users}></DashBoard>
         </>
     )
 }
