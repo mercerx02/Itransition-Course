d160ca182be5ba7c9b7119cf50032921f13b9ea2
mercerx02 <laptonik322@mail.ru>
2023-08-10 12:43:05 +0300

    added new model & update backend

--- Task 6/backend/controllers/messageController.js
+++ Task 6/backend/controllers/messageController.js
@@ -1,39 +1,53 @@
 const asyncHandler = require('express-async-handler')
 
 const Message = require('../models/messageModel')
-
+const Tag = require('../models/TagModel')
 
 const getMessages= asyncHandler(async (req, res) => {
     try {
-        const messges = await Message.find({}, '_id text tags');
-        res.status(200).json(messges)
+        const messages = await Message.find({}, '_id text tags');
+        const tags = await Tag.find({}, 'name');
+
+        res.status(200).json({message: messages, tags: tags})
 
     } catch (error) {
         res.status(400).json({message:'Server error'})
 
     }
 })
+
+
+
 const addMessage = asyncHandler(async (req, res) => {
     try {
+        const { text, tags } = req.body;
 
-        const {text, tags} = req.body
+        let createdTags = [];
+
+        if (tags && tags.length > 0) {
+            createdTags = await Promise.all(
+                tags.map(async tagName => {
+                    const tag = await Tag.create({ name: tagName });
+                    return tag._id;
+                })
+            );
+        }
 
         const message = await Message.create({
             text: text,
-            tags: tags,
+            tags: createdTags,
+        });
 
-        })
-        res.status(200).json({message: message})
+        res.status(200).json({ message: message });
 
     } catch (error) {
-        console.log(error)
-        res.status(400).json({message:'Server error'})
-
+        console.log(error);
+        res.status(400).json({ message: 'Server error' });
     }
-})
+});
 
 
 module.exports = {
     getMessages,
-    addMessage
+    addMessage,
 }
