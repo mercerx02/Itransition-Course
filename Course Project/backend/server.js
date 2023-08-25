f898fcd8e7123570201da8d787b00aeed2b16ec6
mercerx02 <laptonik322@mail.ru>
2023-08-25 22:02:43 +0300

    Update server.js

--- Course Project/backend/server.js
+++ Course Project/backend/server.js
@@ -6,22 +6,31 @@ const app = express()
 const cors = require('cors')
 const passportSetup = require('./OAuth/passport')
 const authRoutes = require('./routes/auth')
+const session = require('express-session')
+
 
 const passport = require('passport')
 
-app.use(cookieSession(
-{
-    name:"session",
-    keys:['mercerx02'],
-    maxAge: 100 * 24 * 60 * 60,
-    sameSite: 'none',
-}))
+// app.use(cookieSession(
+// {
+//     name:"session",
+//     keys:['mercerx02'],
+//     maxAge: 100 * 24 * 60 * 60,
+//     sameSite: 'none',
+// }))
+
+app.use(session({
+    secret: 'keyboard cat',
+    resave: true,
+    saveUninitialized: true,
+    cookie: { secure: true }
+  }))
+
 
 app.use(cors({
-    origin: ["https://merry-cascaron-4c67b3.netlify.app","http://localhost:5173/"],
+    origin: ["https://merry-cascaron-4c67b3.netlify.app","http://localhost:5173"],
     methods:'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
-    exposedHeaders: 'Set-Cookie',
 
 }))
 
